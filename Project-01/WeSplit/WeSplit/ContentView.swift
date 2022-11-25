7262d22b0f64c1d21ac5ea3d0cee3a6f366cf369
M. Zeglam <mjz_inter@hotmail.com>
2022-11-26 01:53:00 +0200

    Project 01 added

---
+++ Project-01/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Mohamed Jamal Ben Zeglam on 11/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var tabCount = 0
+    
+    var body: some View {
+        Text("Hello, world!")
+                    .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-01/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mohamed Jamal Ben Zeglam on 11/25/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
