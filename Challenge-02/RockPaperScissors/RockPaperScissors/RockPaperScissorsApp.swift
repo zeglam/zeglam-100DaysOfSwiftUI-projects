2e2ef2977748e37537ca18be5ada8bb0ca6ef46c
M. Zeglam <mjz_inter@hotmail.com>
2022-12-13 13:31:02 +0200

    Challenge 2 project created
    rock paper scissors game

---
+++ Challenge-02/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Mohamed Jamal Ben Zeglam on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
