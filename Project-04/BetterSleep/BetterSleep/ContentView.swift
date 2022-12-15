cf767b261555beaf87794c370550862d1f2608e5
M. Zeglam <mjz_inter@hotmail.com>
2022-12-15 16:59:43 +0200

    Proj 4 - Part 2: Basic layout built

--- Project-04/BetterSleep/BetterSleep/ContentView.swift
+++ Project-04/BetterSleep/BetterSleep/ContentView.swift
@@ -11,19 +11,44 @@ struct ContentView: View {
     
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        VStack {
-            Spacer()
-            
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...11, step: 0.25)
-            
-            Spacer()
-            
-            DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute) 
-            
-            Spacer()
+        NavigationView {
+            VStack {
+                Spacer()
+                
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Spacer()
+                
+                Text("What is your desired amount of sleep?")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+
+                
+                Spacer()
+                
+                Text("How many cups of coffee will you drink today?")
+                    .font(.headline)
+                
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount.formatted()) cups", value: $coffeeAmount, in: 0...20)
+                
+                Spacer()
+            }
+            .navigationTitle("Better Rest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
         }
+    }
+    func calculateBedTime () {
+        
     }
 }
 
