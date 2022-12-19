d1eb22df752361112027268eebb2832961390127
M. Zeglam <mjz_inter@hotmail.com>
2022-12-20 01:04:05 +0200

    proj 5 - part 2: building basic app layout

--- Project-05/WordScramble/WordScramble/ContentView.swift
+++ Project-05/WordScramble/WordScramble/ContentView.swift
@@ -8,23 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        List {
-            Section("Sec 1") {
-                Text("Static Row Number 1")
-            }
-
-            Section("Sec2") {
-                ForEach(0..<3) {
-                    Text("Dynamic Row Number \($0)")
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
                 }
             }
-            
-            Section("Sec 3") {
-                Text("Static Row Number 2")
-            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord) //onSubmit means when user presses return
         }
-        .listStyle(.grouped)
+    }
+    func addNewWord() {
+        let answer = newWord.lowercased() .trimmingCharacters(in:  .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        } 
+        newWord = ""
     }
 }
 
