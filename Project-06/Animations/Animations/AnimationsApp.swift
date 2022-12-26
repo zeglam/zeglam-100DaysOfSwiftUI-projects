313910502bcca38035ad0f0e89e3e7cab3b6a83d
M. Zeglam <mjz_inter@hotmail.com>
2022-12-26 12:02:42 +0200

    proj 6 - part 1: created

---
+++ Project-06/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Mohamed Jamal Ben Zeglam on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
