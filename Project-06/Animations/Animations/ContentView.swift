9f64a4a80b9626471af47ec3e02c659f52282f03
M. Zeglam <mjz_inter@hotmail.com>
2022-12-26 14:49:22 +0200

    proj 6 - part 1: exploring

--- Project-06/Animations/Animations/ContentView.swift
+++ Project-06/Animations/Animations/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animationFactor = 1.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Stepper("Scale Amount", value: $animationFactor.animation(), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tape Me") {
+                animationFactor += 1
+            }
+            .padding(30)
+            .background(.blue)
+            .foregroundColor(.orange)
+            .clipShape(Circle())
+            .scaleEffect(animationFactor)
+        }
     }
 }
 
