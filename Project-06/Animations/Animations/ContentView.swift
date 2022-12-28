dda75158857a7193826ce1838e0596af0aaf988b
M. Zeglam <mjz_inter@hotmail.com>
2022-12-28 22:08:30 +0200

    proj 6 - part 2: custom transitions

--- Project-06/Animations/Animations/ContentView.swift
+++ Project-06/Animations/Animations/ContentView.swift
@@ -7,40 +7,80 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
     
 //    @State private var animationFactor = 0.0
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
-    let letters = Array("Hello, SwiftUI")
+//    @State private var enabled = false
+//    @State private var dragAmount = CGSize.zero
+//    let letters = Array("Hello, SwiftUI")
+    
+    @State private var isShowingRed = false
     
     var body: some View {
         
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count) {
-                num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .teal : .red)
-                    .offset(dragAmount)
-                    .animation(
-                        .default.delay(Double(num) / 20),
-                        value: dragAmount
-                    )
+        ZStack {
+            Rectangle()
+                .fill(.mint)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged {
-                    dragAmount = $0.translation
-                }
-                .onEnded {
-                    _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                }
-        )
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+        
+        
+//        HStack(spacing: 0) {
+//            ForEach(0..<letters.count) {
+//                num in
+//                Text(String(letters[num]))
+//                    .padding(5)
+//                    .font(.title)
+//                    .background(enabled ? .teal : .red)
+//                    .offset(dragAmount)
+//                    .animation(
+//                        .default.delay(Double(num) / 20),
+//                        value: dragAmount
+//                    )
+//            }
+//        }
+//        .gesture(
+//            DragGesture()
+//                .onChanged {
+//                    dragAmount = $0.translation
+//                }
+//                .onEnded {
+//                    _ in
+//                    dragAmount = .zero
+//                    enabled.toggle()
+//                }
+//        )
         
 //        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
 //            .frame(width: 300, height: 200, alignment: .leading)
