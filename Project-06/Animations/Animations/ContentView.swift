b34704714e479e0195e612b07272bc349ceb9bf4
M. Zeglam <mjz_inter@hotmail.com>
2022-12-28 18:34:03 +0200

    proj 6 - part 2: multiple animations

--- Project-06/Animations/Animations/ContentView.swift
+++ Project-06/Animations/Animations/ContentView.swift
@@ -10,23 +10,33 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var animationFactor = 0.0
+    @State private var enabled = false
     
     var body: some View {
         VStack {
-            Button("Tape Me") {
-                withAnimation{
-                    animationFactor += 360.0
-                }
+//            Button("Tape Me") {
+//                withAnimation{
+//                    animationFactor += 360.0
+//                }
+//            }
+//            .padding(70)
+//            .background(.orange)
+//            .foregroundColor(.teal)
+//            .font(.largeTitle)
+//            .clipShape(Circle())
+//            .rotation3DEffect(
+//                .degrees(animationFactor),
+//                axis: (x: 0, y: 0.5, z: 0.5)
+//            )
+            Button("Tape me now!") {
+                enabled.toggle()
             }
-            .padding(70)
-            .background(.orange)
-            .foregroundColor(.teal)
-            .font(.largeTitle)
-            .clipShape(Circle())
-            .rotation3DEffect(
-                .degrees(animationFactor),
-                axis: (x: 0, y: 0.5, z: 0.5)
-            )
+            .frame(width: 200, height: 200, alignment: .leading)
+            .background(enabled ? .teal : .red)
+            .foregroundColor(.gray)
+            .animation(.default, value: enabled)
+            .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+            .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
         }
     }
 }
