ad6f5fde3dad5a71fbe29099f7f3b8b756c8a544
M. Zeglam <mjz_inter@hotmail.com>
2023-01-02 20:18:57 +0200

    Challenge 03: edutainment creation

---
+++ Challenge-03/MultiplicationEdutainment/MultiplicationEdutainment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplicationEdutainment
+//
+//  Created by Mohamed Jamal Ben Zeglam on 1/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Challenge-03/MultiplicationEdutainment/MultiplicationEdutainment/MultiplicationEdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationEdutainmentApp.swift
+//  MultiplicationEdutainment
+//
+//  Created by Mohamed Jamal Ben Zeglam on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationEdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
