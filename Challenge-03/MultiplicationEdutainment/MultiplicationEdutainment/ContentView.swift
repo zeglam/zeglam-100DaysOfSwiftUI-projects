cd71f95edcf785dc8dc9bec5f37d1a41a1fc5d78
M. Zeglam <mjz_inter@hotmail.com>
2023-01-03 20:02:19 +0200

    challenge 3: going

--- Challenge-03/MultiplicationEdutainment/MultiplicationEdutainment/ContentView.swift
+++ Challenge-03/MultiplicationEdutainment/MultiplicationEdutainment/ContentView.swift
@@ -18,35 +18,93 @@ struct ContentView: View {
     @State private var userAnswer = 0
     //number of queastions player will choose
     @State private var numberOfQuestions = 0
+    @State private var questionList: [(Int, Int, Int)] = []
     
     var body: some View {
-        VStack {
-            Text("Welcome to Multiplication Adventures")
+        ZStack {
+            LinearGradient(colors: [.teal, .pink], startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
             
-            Form {
-                Section {
-                    Stepper("\(firstNumber)", value: $firstNumber, in: 1...12, step: 1)
-                } header: {
-                    Text("Select multiplication table")
-                }
-                
-                Section {
-                Picker("Select the number of questions", selection: $numberOfQuestions) {
-                    ForEach(0..<4, id: \.self) { num in
-                        Text("\((num + 1) * 5)")
+            //use if/else to create game page and settings page
+            if !gameON {
+                VStack {
+                    Text("Welcome to Multiplication Adventures")
+                    
+                    Form {
+                        Section {
+                            Stepper("\(firstNumber)", value: $firstNumber, in: 1...12, step: 1)
+                        } header: {
+                            Text("Select multiplication table")
+                        }
+                        
+                        Section {
+                        Picker("Select the number of questions", selection: $numberOfQuestions) {
+                            ForEach(0..<4, id: \.self) { num in
+                                Text("\((num + 1) * 5)")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                        } header: {
+                            Text("Select the number of questions")
+                        }
+                        
                     }
+                    //the following modifiers are to change the background color of our form only, otherwise its going to be white
+                    .onAppear {
+                        UITableView.appearance().backgroundColor = .clear
+                    }
+                    .onDisappear {
+                        UITableView.appearance().backgroundColor = .systemGroupedBackground
+                    }
+                    .background(.yellow)
+                    
+                    Button("Start the game") {
+                        //create array of questions
+                        questionList = createQuestions(table: firstNumber, numOfQuestions: numberOfQuestions)
+                        //change to game page
+                        gameON.toggle()
+                    } .buttonStyle(.borderedProminent)
                 }
-                .pickerStyle(.segmented)
-                } header: {
-                    Text("Select the number of questions")
-                }
-                
+                .background(.yellow)
             }
             
-            Button("Start the game") {
-                //game code
-            } .buttonStyle(.borderedProminent)
+            else {
+                //code for game screen
+                VStack {
+                    Text("Give the correct answer")
+                    
+                    HStack {
+                        ForEach(questionList, id: \.self) { question in
+                            Text("\(firstNumber)")
+                            Text("X")
+                            Text("\(question.1.content)")
+                        }
+                    }
+                }
+            }
         }
+        
+    }
+    
+    //this func creates a list of questions in form of an array of tuples, each tuple has 3 numbers, first is the table choosen by user, second is a random number from 1 to 12, third is multiplication of first and second numbers. This func should be called once the user hits play button.
+    func createQuestions(table: Int, numOfQuestions: Int) -> () {
+        var questions: [(Int, Int, Int)] = []
+        for _ in 1...numOfQuestions {
+            questions.append((table, Int.random(in: 1...12), table * Int.random(in: 1...12)))
+        }
+        return questions
+    }
+}
+
+//create Question struct, to be the file type to store questions and their answers
+struct Question {
+    var num1: Int
+    var num2: Int
+    var correctAnswer: Int {
+        return num1 * num2
+    }
+    var questionText: String {
+        return "\(num1) X \(num2) = "
     }
 }
 
