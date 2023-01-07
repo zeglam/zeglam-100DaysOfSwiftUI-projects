3b27c0b3dd079e73c10805e3bc75876fa40df4dd
M. Zeglam <mjz_inter@hotmail.com>
2023-01-07 15:55:41 +0200

    challenge 3: create Question struct

--- Challenge-03/MultiplicationEdutainment/MultiplicationEdutainment/ContentView.swift
+++ Challenge-03/MultiplicationEdutainment/MultiplicationEdutainment/ContentView.swift
@@ -9,23 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     
-    //gameOn = true if player is playing, false if player setting up game
+    //gameOn: true if player is playing, false if player setting up game
     @State private var gameON = false
-    //list of all mumbers concidered in the game
+    //list of all mumbers concidered in the game, may not be needed
     @State private var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     @State private var firstNumber = 1
     @State private var secondNumber = 1
     @State private var userAnswer = 0
     //number of queastions player will choose
-    @State private var numberOfQuestions = 0
-    @State private var questionList: [(Int, Int, Int)] = []
+    @State private var numberOfQuestions = 5
+    @State private var questionList: [Question] = []
     
     var body: some View {
         ZStack {
             LinearGradient(colors: [.teal, .pink], startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
             
-            //use if/else to create game page and settings page
+            //use if/else to create a game page and a settings page
             if !gameON {
                 VStack {
                     Text("Welcome to Multiplication Adventures")
@@ -59,7 +59,7 @@ struct ContentView: View {
                     .background(.yellow)
                     
                     Button("Start the game") {
-                        //create array of questions
+                        //create an array of questions
                         questionList = createQuestions(table: firstNumber, numOfQuestions: numberOfQuestions)
                         //change to game page
                         gameON.toggle()
@@ -67,17 +67,17 @@ struct ContentView: View {
                 }
                 .background(.yellow)
             }
-            
+            //Now lets construct game page
             else {
-                //code for game screen
+                //code for game page
                 VStack {
                     Text("Give the correct answer")
                     
                     HStack {
-                        ForEach(questionList, id: \.self) { question in
+                        ForEach(0..<questionList.count, id: \.self) { question in
                             Text("\(firstNumber)")
                             Text("X")
-                            Text("\(question.1.content)")
+                            Text("\(questionList[question].num2)")
                         }
                     }
                 }
@@ -86,25 +86,24 @@ struct ContentView: View {
         
     }
     
-    //this func creates a list of questions in form of an array of tuples, each tuple has 3 numbers, first is the table choosen by user, second is a random number from 1 to 12, third is multiplication of first and second numbers. This func should be called once the user hits play button.
-    func createQuestions(table: Int, numOfQuestions: Int) -> () {
-        var questions: [(Int, Int, Int)] = []
-        for _ in 1...numOfQuestions {
-            questions.append((table, Int.random(in: 1...12), table * Int.random(in: 1...12)))
+    //this func creates a list of questions in form of an array of Question struct we created below, each instance has 3 numbers, first is the table choosen by user, second is a random number from 1 to 12, third is multiplication of first and second numbers. This func should be called once the user hits play button.
+    func createQuestions(table: Int, numOfQuestions: Int) -> [Question] {
+        var questions:[Question] = []
+        //using stride() instide of 0..<numQuestions since looping in a layout gives error
+        for _ in stride(from: 1, through: numOfQuestions, by: 1) {
+            let tempQuestion = Question(num1: table, num2: Int.random(in: 1...12))
+            questions.append(tempQuestion)
         }
         return questions
     }
 }
 
-//create Question struct, to be the file type to store questions and their answers
+//create Question struct, to be the file type to store questions and their correct answers
 struct Question {
     var num1: Int
     var num2: Int
-    var correctAnswer: Int {
-        return num1 * num2
-    }
     var questionText: String {
-        return "\(num1) X \(num2) = "
+        return "\(num1) X \(num2) = \(num1 * num2)"
     }
 }
 
